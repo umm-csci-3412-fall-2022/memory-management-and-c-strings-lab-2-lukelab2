4ad4184e03f30f0aa5f51e94da44dc83dcac3ea0
burde041 <burde041@morris.umn.edu>
2022-09-26 08:36:12 -0500

    Fix memory leak in palindrome main.c

--- palindrome/main.c
+++ palindrome/main.c
@@ -20,8 +20,9 @@ int main(int argc, char *argv[]) {
     // Without it, nothing will ever be a palindrome, unless it happens
     // to somehow start with a '\n'.
     line[strcspn(line, "\n")] = '\0';
-    printf("Is the string <%s> a palindrome? %s\n", line, palindrome(line));
+    char *a = palindrome(line);
+    printf("Is the string <%s> a palindrome? %s\n", line, a);
+    free(a);
   }
-
   free(line);
 }
